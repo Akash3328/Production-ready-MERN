9b8afd229951e51455708d8d9595c5d90e4ea5e5
Akash Odedara <akashodedara7@gmail.com>
2026-05-12 22:18:46 +0530

    Add server setup with Express and CORS, and define API route

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// API route
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from sky farm " });
+});
+
+const PORT = 4000;
+app.listen(PORT, () =>
+  console.log(`Server is running at http://localhost:${PORT} `),
+);
